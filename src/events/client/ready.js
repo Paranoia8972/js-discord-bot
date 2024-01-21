8c8db3fc07051bd6dfbf996aeb35d34971e3250b
Paranoia8972 <contact@encryptopia.dev>
2024-01-21 22:39:44 +0100

    Update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,9 +1,24 @@
 const { Client } = require("discord.js");
+const mongoose = require("mongoose");
+const mongoURL = process.env.mongoURL;
 
 module.exports = {
-    name: 'ready',
-    once: true,
-    async execute(client) {
-        console.log(`Ready!!! ${client.user.tag} is logged in and online. ;)`);
-    }   
-}
+  name: "ready",
+  once: true,
+  async execute(client) {
+    console.log(`Ready!!! ${client.user.tag} is logged in and online. ;)`);
+
+    if (!mongoURL) return;
+
+    await mongoose.connect(mongoURL || "", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    if (mongoose.connect) {
+      console.log(`Connected to the Database!`);
+    } else {
+      console.log(`Connection to Database failed!`);
+    }
+  },
+};
